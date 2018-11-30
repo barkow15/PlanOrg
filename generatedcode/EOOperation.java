b56dd46edd7d04c29bf69f309f460e33fda9a544
Martin Kjeldgaard Nikolajsen <mart56p7@stud.kea.dk>
2018-11-30 10:20:27 +0100

    m

--- generatedcode/EOOperation.java
+++ generatedcode/EOOperation.java
@@ -1,21 +1,25 @@
 public enum EOOperation {
-   START (null, "Start menu", EODisplayType.START),
-   STARTSHOWALL (null, "Start menu", EODisplayType.START),      
-   CREATEARRANGEMENT (null, "Opret Arrangement", EODisplayType.CREATEARRANGEMENT),
-   UPDATEARRANGEMENT (null, "Opdater Arrangement", EODisplayType.UPDATEARRANGEMENT),
-   DELETEARRANGEMENT (null, "Slet Arrangement", EODisplayType.DELETEARRANGEMENT),
-   OPENARRANGEMENT (null, "Vis Arrangement", EODisplayType.OPENARRANGEMENT),   
-   CREATEEVENT (null, "Opret Begivenhed", EODisplayType.CREATEEVENT),
-   UPDATEEVENT (null, "Opdater Begivenhed", EODisplayType.UPDATEEVENT),
-   DELETEEVENT (null, "Slet Begivenhed", EODisplayType.DELETEEVENT),   
-   EXPORT (null, "Exporter", EODisplayType.EXPORT),
-   ADMFACILITATOR (null, "Administrer Facilitatorer", EODisplayType.ADMFACILITATOR),
-   ADMEVENTTYPE (null, "Administrer Begivenhedstyper", EODisplayType.ADMEVENTTYPE),
-   ERROR (null, "Fejl sk�rm", EODisplayType.ERROR),
-   SAVECSV (null, "Gem CSV", EODisplayType.START),
-   SAVECREATEEVENT (null, "Gem Event", EODisplayType.START),
-   UPDATEFACILITATOR (null,"Rediger Facilitator",EODisplayType.ADMFACILITATOR);
-   
+   START (null, "Start menu", EODisplayType.START), //Martin - Mangler DB
+   STARTSHOWALL (null, "Start menu", EODisplayType.START), //Martin - Mangler DB
+   CREATEARRANGEMENT (null, "Opret Arrangement", EODisplayType.CREATEARRANGEMENT), //Marcus
+   UPDATEARRANGEMENT (null, "Opdater Arrangement", EODisplayType.UPDATEARRANGEMENT), //Marcus
+   DELETEARRANGEMENT (null, "Slet Arrangement", EODisplayType.DELETEARRANGEMENT), //Marcus
+   OPENARRANGEMENT (null, "Vis Arrangement", EODisplayType.OPENARRANGEMENT), //Martin - Mangler DB
+   CREATEEVENT (null, "Opret Begivenhed", EODisplayType.CREATEEVENT),  //Rasmus
+   UPDATEEVENT (null, "Opdater Begivenhed", EODisplayType.UPDATEEVENT),  //Rasmus
+   DELETEEVENT (null, "Slet Begivenhed", EODisplayType.DELETEEVENT), //Rasmus
+   SAVECREATEEVENT (null, "Gem Event", EODisplayType.START), //Rasmus
+   IMPORT (null, "Importer", EODisplayType.IMPORT), //Martin - DONE
+   IMPORTCSV (null, "Importer", EODisplayType.START), //Faelles  
+   EXPORT (null, "Exporter", EODisplayType.EXPORT), //Martin - DONE
+   SAVECSV (null, "Gem CSV", EODisplayType.START), //Faelles 
+   ADMFACILITATOR (null, "Administrer Facilitatorer", EODisplayType.ADMFACILITATOR), //Frederik
+   UPDATEFACILITATOR (null,"Rediger Facilitator",EODisplayType.ADMFACILITATOR), //Frederik
+   CREATEFACILITATOR (null,"Opret Facilitator",EODisplayType.ADMFACILITATOR), //Frederik
+   DELETEFACILITATOR(null,"Slet facilitator",EODisplayType.ADMFACILITATOR), //Frederik
+   ADMEVENTTYPE (null, "Administrer Begivenhedstyper", EODisplayType.ADMEVENTTYPE), //Martin
+   ERROR (null, "Fejl Skaerm", EODisplayType.ERROR); //Martin
+ 
    private Object data = null;
    private EODisplayType displaytype;
    private String displayname;
