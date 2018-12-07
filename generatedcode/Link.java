516b17307c62e7baa5815424e8e333515e7c1fdd
Martin Kjeldgaard Nikolajsen <martinnikolajsen@outlook.dk>
2018-12-07 09:51:54 +0100

    m

--- generatedcode/Link.java
+++ generatedcode/Link.java
@@ -61,4 +61,46 @@ public class Link implements EOCSVInterface
       }
       return(false);
    }
+   
+   @Override      
+   public int hashCode()
+   {
+      String[] tables = new String[12];
+      tables[0] = "EOArrangements";
+      tables[1] = "EOArrangements_has_EOContactInfo";
+      tables[2] = "EOArrangements_has_EOEvents";
+      tables[3] = "EOArrangements_has_EOFacilitatorContactInfo";
+      tables[4] = "EOCustomerContactInfo";
+      tables[5] = "EOEvents";
+      tables[6] = "EOEvents_has_EOEventtypes";
+      tables[7] = "EOEvents_has_EOFacilitatorContactInfo";
+      tables[8] = "EOEventtypes";
+      tables[9] = "EOEventtypes_has_EOExternalContactInfo";
+      tables[10] = "EOExternalContactInfo";
+      tables[11] = "EOFacilitatorContactInfo";
+      
+      int[] tablesvalue = new int[12];
+      tablesvalue[0] = 1550000001;
+      tablesvalue[1] = 1600000001;
+      tablesvalue[2] = 1650000001;
+      tablesvalue[3] = 1700000001;
+      tablesvalue[4] = 1750000001;
+      tablesvalue[5] = 1800000001;
+      tablesvalue[6] = 1850000001;
+      tablesvalue[7] = 1900000001;
+      tablesvalue[8] = 1950000001;
+      tablesvalue[9] = 2000000001;
+      tablesvalue[10] = 2050000001;
+      tablesvalue[11] = 2100000001;
+      
+      int value = 1550000001;
+      for(int i = 0; i < 12; i++)
+      {
+         if(table.equals(tables[i]))
+         {
+            value = tablesvalue[1] + value1 * 1000 + value2;
+         }
+      }         
+      return(value);
+   }    
 }
